663193f06b69a91186b1d3281fe10a39ecf2efe6
Chris Wong <yuhu198275@gmail.com>
2022-02-03 17:23:29 +0800

    init

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -8,10 +8,10 @@ const ContactMe = () => {
             <h1 style={{width: "100%", textAlign: "center", marginBottom: "50px", marginTop: "20px", color: "#dee2e6"}}>Contact Me</h1>
             <Row style={{justifyContent: "center", alignItems: "center", height: "100%"}}>
                 <Col sm={6} md={3} lg={3} style={{display: 'flex', justifyContent: 'center'}}>
-                    <SocialIcon url='#'/> {/* N/A */}
+                    <SocialIcon url='https://twitter.com/'/> {/* N/A */}
                 </Col>
                 <Col sm={6} md={3} lg={3} style={{display: 'flex', justifyContent: 'center'}}>
-                    <SocialIcon url='#' /> {/* N/A */}
+                    <SocialIcon url='https://instagram.com/'/>  {/* N/A */}
                 </Col>
                 <Col sm={6} md={3} lg={3} style={{display: 'flex', justifyContent: 'center'}}>
                     <SocialIcon url='https://www.linkedin.com/in/chun-kit-koon-08016322a/' />
